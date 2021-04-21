dbca7a2f325789305ed828381ba9ffc426c6432f
Bastien Penard <bpenard@lucca.fr>
2021-04-21 14:39:59 +0000

    exo 4

--- 02-ruby-basics/04-listing-activities/listing.rb
+++ 02-ruby-basics/04-listing-activities/listing.rb
@@ -1,5 +1,18 @@
 activities = [
-  # TODO: gather at least 3 activities
+  { "name" => "Apero", "Category" => "Bonne Ambiance", "City" => "Paris", "Duration" => 180, "Number of participant" => 15, "Price" => 100},
+  { "name" => "Perudo", "Category" => "Jeu", "City" => "Nantes", "Duration" => 35, "Number of participant" => 5, "Price" => 10},
+  { "name" => "Foot", "Category" => "Sport", "City" => "Marseille", "Duration" => 90, "Number of participant" => 22, "Price" => 1000}
 ]
 
 # TODO: your code goes here
+ p activities
+
+activities.each do |artist|
+  puts "#{artist["name"]} [ #{artist["Category"].upcase} ]"
+  puts "Located in #{artist["City"]}"
+  puts "Duration of #{artist["Duration"]} minutes"
+  puts "For #{artist["Number of participant"]} participants"
+  puts "Charged #{artist["Price"]}€"
+
+  puts "---"
+end
