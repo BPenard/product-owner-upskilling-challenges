62e82f8f2cfc064743d96eab4bab1a04231e24fa
Bastien Penard <bpenard@lucca.fr>
2021-04-21 13:38:09 +0000

    First exercice done easily

--- 02-ruby-basics/01-what-is-your-age/age.rb
+++ 02-ruby-basics/01-what-is-your-age/age.rb
@@ -1,6 +1,20 @@
 puts "What is your name?"
 name = gets.chomp # asks user to type something in the Terminal
 
+
+
 puts "Ok got it!"
 
 # TODO: your code goes here
+ puts "What's your year of birth ? "
+ birthyear = gets.chomp
+
+puts "OK your birth year is #{birthyear}"
+birthyear = birthyear.to_i
+
+# p birthyear.class
+
+
+age = 2021 - birthyear
+
+puts "Hey #{name}! You are #{age} years old this year."
