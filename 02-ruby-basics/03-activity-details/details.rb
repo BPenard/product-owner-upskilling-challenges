d932b3b1014fbff6f01b239ef8cff2ff9e12c6f3
Bastien Penard <bpenard@lucca.fr>
2021-04-21 14:22:37 +0000

    3rd exo

--- 02-ruby-basics/03-activity-details/details.rb
+++ 02-ruby-basics/03-activity-details/details.rb
@@ -1,5 +1,11 @@
-activity = {
+activity = { "name" => "Apero", "Category" => "Bonne Ambiance", "City" => "Paris", "Duration" => 180, "Number of participant" => 15, "Price" => 100
   # TODO: gather the characteristics of your preferred activity
 }
-
+#p activity
 # TODO: your code goes here
+
+puts "#{activity["name"]} [ #{activity["Category"].upcase} ]"
+puts "Located in #{activity["City"]}"
+puts "Duration of #{activity["Duration"]} minutes"
+puts "For #{activity["Number of participant"]} participants"
+puts "Charged #{activity["Price"]}€"
