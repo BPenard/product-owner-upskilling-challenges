8afa9eaec254b3790db3a6ce0a54812d5c937b1b
Bastien Penard <bpenard@lucca.fr>
2021-04-21 13:57:47 +0000

    2nd exercice

--- 02-ruby-basics/02-listing-categories/categories.rb
+++ 02-ruby-basics/02-listing-categories/categories.rb
@@ -1,5 +1,21 @@
-categories = [
+categories = ["Adventure", "Sport", "Visits", "Workshops", "Cultural", "Food"]
   # TODO: gather the categories of activity
-]
+
+#p categories
 
 # TODO: your code goes here
+
+puts "Hey! Can you enter a new teambuilding categorie?"
+newcategory = gets.chomp
+categories.push (newcategory)
+
+categories = categories.sort
+
+number = categories.count
+
+categories.each do |name|
+  puts "- #{name}"
+
+end
+
+puts "Total: #{number}"
