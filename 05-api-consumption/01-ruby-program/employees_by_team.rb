19710fab637ddb654ec190a68f9688330fd90a46
Bastien Penard <bpenard@lucca.fr>
2021-04-22 15:54:03 +0000

    API search user

--- 05-api-consumption/01-ruby-program/employees_by_team.rb
+++ 05-api-consumption/01-ruby-program/employees_by_team.rb
@@ -6,4 +6,28 @@ team = gets.chomp # asks user to type something in the Terminal
 
 puts "Ok got it! Let's search for the employees..."
 
-# TODO: your code goes here
+#team ="UI"
+
+url ="https://team-building-api.cleverapps.io/v2/teams"
+response = RestClient.get(url , params: {"search"=> team })
+response = JSON.parse (response.body)
+
+#p"response"
+#p response
+
+found_team = response["teams"][0]
+
+
+
+url ="https://team-building-api.cleverapps.io/v2/employees"
+response = RestClient.get(url , params: {"team_id"=> found_team["id"]})
+employees = JSON.parse (response.body)
+
+employees = employees["employees"]
+
+employees.each do |person|
+  puts "#{person["first_name"]} #{person["last_name"]}"
+  puts "#{person["job_title"]}"
+
+  puts "---"
+end
--- 05-api-consumption/01-ruby-program/search_employee.rb
+++ 05-api-consumption/01-ruby-program/search_employee.rb
@@ -6,4 +6,22 @@ name = gets.chomp # asks user to type something in the Terminal
 
 puts "Ok got it! Let's search for that person..."
 
-# TODO: your code goes here
+
+first_name = name.split[0]
+last_name = name.split[1]
+
+
+#first_name = "Laura"
+#last_name = "Olive"
+
+
+url ="https://team-building-api.cleverapps.io/v2/employees"
+response = RestClient.get(url , params: {"first_name"=> first_name , "last_name"=> last_name})
+employees = JSON.parse (response.body)
+
+
+user = employees["employees"][0]
+
+puts "#{user["first_name"]} #{user["last_name"]} (#{user["job_title"]})"
+puts "From team #{user["team"]["name"]}"
+puts "Working from #{user["site"]["name"]} "
