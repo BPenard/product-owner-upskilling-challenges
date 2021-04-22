2b39cc5f7661162ee4246bd5ecdbfd928db9567b
Bastien Penard <bpenard@lucca.fr>
2021-04-22 10:04:16 +0000

    sinatra exo

--- 03-sinatra-web-app/app.rb
+++ 03-sinatra-web-app/app.rb
@@ -6,8 +6,12 @@ enable :static
 
 get "/" do
   @activities = [
-    # TODO: gather a few activities
+    { "name" => "Apero", "Category" => "Bonne Ambiance", "City" => "Paris", "Duration" => 180, "Number of participant" => 15, "Price" => 100},
+    { "name" => "Perudo", "Category" => "Jeu", "City" => "Nantes", "Duration" => 35, "Number of participant" => 5, "Price" => 10},
+    { "name" => "Foot", "Category" => "Sport", "City" => "Marseille", "Duration" => 90, "Number of participant" => 22, "Price" => 1000}
+
   ]
+
 
   erb :index
 end
