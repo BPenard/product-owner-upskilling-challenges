d7c396f78ea9c0d0bcb0fd61eaa02cecafd53c40
Bastien Penard <bpenard@lucca.fr>
2021-04-23 12:08:13 +0000

    exo

--- 05-api-consumption/02-sinatra-web-app/app.rb
+++ 05-api-consumption/02-sinatra-web-app/app.rb
@@ -7,12 +7,29 @@ require "sinatra/reloader" if development?
 
 enable :static
 
+
+
+
+
 get "/" do
-  @activities = # TODO: retrieve all the activities from the API
+  url = "https://team-building-api.cleverapps.io//v2/activities"
+  response = RestClient.get(url, params: {"city"=>params["location"]})
+  payload = JSON.parse (response.body)
+  @activities = payload["activities"]
+
 
   erb :index
 end
 
 get "/components" do
   erb :components
+end
+
+get "/activities/:id" do
+  url = "https://team-building-api.cleverapps.io//v2/activities/#{params["id"]}"
+  response = RestClient.get(url)
+  payload = JSON.parse (response.body)
+  @activity = payload["activity"]
+
+  erb :details
 end
